61d749c4f3001fb4198c67506944f1895b022a45
Girish Kumar Shakya <clairvoyant.1947@gmail.com>
2018-12-27 17:43:57 +0530

    Initial commit

---
+++ app/src/main/java/helloworld/com/seekbardemo/MainActivity.java
@@ -0,0 +1,35 @@
+package helloworld.com.seekbardemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private SeekBar seekBar;
+    private TextView textView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        seekBar = findViewById(R.id.seekBar);
+        textView = findViewById(R.id.textView);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int increment = i;
+                textView.setText("Seek To : "+increment);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
